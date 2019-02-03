0d024990894634b7dc1f04efd0438d3b06809089
HJ-Cai <lj_chj@126.com>
2019-02-03 14:25:08 +0800

    缓存线程安全

--- TestBB/TestBB/DetailList/DetailListCache.m
+++ TestBB/TestBB/DetailList/DetailListCache.m
@@ -44,31 +44,41 @@ static NSLock *lock;
     NSURL *to = [NSURL fileURLWithPath:toStr];
     NSError *error = nil;
     [[NSFileManager defaultManager] moveItemAtURL:from toURL:to error:&error];
+    [lock lock];
     [self.caches setObject:path.lastPathComponent forKey:@"k_torrent"];
+    [lock unlock];
     [self synchronize];
     return toStr;
 }
 
 - (NSString *)storedTorrent {
+    [lock lock];
     NSString *filename = [self.caches objectForKey:@"k_torrent"];
+    [lock unlock];
     if (!filename) return nil;
     NSString *filePath = [NSString stringWithFormat:@"%@%@",NSTemporaryDirectory(),filename];
     return [[NSFileManager defaultManager] fileExistsAtPath:filePath] ? filePath : nil;
 }
 
 - (void)storeImage:(NSString *)path forName:(NSString *)name {
+    [lock lock];
     [self.caches setObject:path forKey:name];
+    [lock unlock];
     [self synchronize];
 }
 
 - (NSString *)storedImageForName:(NSString *)name {
+    [lock lock];
     NSString *imgPath =[self.caches objectForKey:name];
+    [lock unlock];
     return [[NSFileManager defaultManager] fileExistsAtPath:imgPath] ? imgPath : nil;
 }
 
 - (void)synchronize {
+    [lock lock];
     [[NSUserDefaults standardUserDefaults] setObject:self.caches forKey:self.url];
     [[NSUserDefaults standardUserDefaults] synchronize];
+    [lock unlock];
 }
 
 
